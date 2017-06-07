ced4cfc5ee7829f90fad7576a5471623169ec048
ankursoni <me.soni88@gmail.com>
2017-06-07 20:03:27 +0530

    high level component structure created

--- ngg-cms/src/app/app.module.ts
+++ ngg-cms/src/app/app.module.ts
@@ -1,26 +1,69 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { MainNavComponent } from './main-nav/main-nav.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { DashWidgetComponent } from './dashboard/dash-widget/dash-widget.component';
+import {AppComponent} from './app.component';
+import {MainNavComponent} from './main-nav/main-nav.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {DashWidgetComponent} from './dashboard/dash-widget/dash-widget.component';
+import {GridsterModule} from 'angular-gridster2/dist/index';
+import {NewsComponent} from './news/news.component';
+import {RouterModule}   from '@angular/router';
+import {KpiComponent} from './kpi/kpi.component';
+import {ProjectComponent} from './project/project.component';
+import {SevenKeysComponent} from './seven-keys/seven-keys.component';
+import { NewsOverviewComponent } from './news/news-overview/news-overview.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainNavComponent,
-    DashboardComponent,
-    DashWidgetComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations : [
+        AppComponent,
+        MainNavComponent,
+        DashboardComponent,
+        DashWidgetComponent,
+        NewsComponent,
+        KpiComponent,
+        ProjectComponent,
+        SevenKeysComponent,
+        NewsOverviewComponent
+    ],
+    imports : [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        GridsterModule,
+
+        RouterModule.forRoot([
+            {
+                path : 'dashboard',
+                component : DashboardComponent
+            },
+            {
+                path : 'news',
+                component : NewsComponent
+            },
+            {
+                path : 'kpis',
+                component : KpiComponent
+            },
+            {
+                path : 'sevenkeys',
+                component : SevenKeysComponent
+            },
+            {
+                path : 'projects',
+                component : ProjectComponent
+            },
+            {
+                path : '',
+                redirectTo : '/dashboard',
+                pathMatch : 'full'
+            }
+        ])
+
+    ],
+    providers : [],
+    bootstrap : [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
