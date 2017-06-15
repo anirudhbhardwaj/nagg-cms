9eafb60c877860ff3841ac755ce91d7318733571
ankursoni <me.soni88@gmail.com>
2017-06-15 13:36:56 +0530

    node modules deleted

---
+++ ngg-cms/src/app/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../auth.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+    console.log("init auth service");
+  }
+
+  login() {
+    console.log("in login component");
+    if(this.authService.login()){
+      console.log("navigating to dashboard");
+      this.router.navigate(['./dashboard'])
+    }
+  }
+}
