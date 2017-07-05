0d0c476fa3ce562dac30c9ad5d7a9aa1b932e57a
Bhavya Dhingra <bhavya.dhingra@nagarro.com>
2017-07-05 14:14:34 +0530

    Image usgae updated

--- ngg-cms/src/app/news/news-detail/news-detail.component.ts
+++ ngg-cms/src/app/news/news-detail/news-detail.component.ts
@@ -3,6 +3,11 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { NewsService } from '../news-service.service';
 import { News } from '../news.models';
 
+/**
+ * @Component 
+ * @desc This component is used to show user the complete details of a news
+ * @author : Bhavya Dhingra
+ */
 @Component({
   selector: 'app-news-detail',
   templateUrl: './news-detail.component.html',
@@ -12,8 +17,8 @@ export class NewsDetailComponent implements OnInit {
 
   constructor(private newsService: NewsService, private route: ActivatedRoute,
     private router: Router) { }
-
   news: News = new News();
+
   ngOnInit() {
     this.route.paramMap.map((params: ParamMap) =>
       this.newsService.getNewsById(params.get('id')))
