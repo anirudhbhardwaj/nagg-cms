518ad5c3dcc0f61686977469e1abaa6fd42fce12
sacgithub <sachin31may@gmail.com>
2017-07-06 12:33:59 +0530

    Update component

--- ngg-cms/src/app/archived-news/archived-news.component.ts
+++ ngg-cms/src/app/archived-news/archived-news.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./archived-news.component.css']
 })
 export class ArchivedNewsComponent implements OnInit {
+  today =  new Date;
+  date1 : Date;
+  date2 : Date;
+  date3 : Date;
+  date4 : Date;
+  date5 : Date;
 
   constructor() { }
 
   ngOnInit() {
+    this.date1 = new Date(this.today.getFullYear(), this.today.getMonth() , 0);
+    this.date2 = new Date(this.today.getFullYear(), this.today.getMonth() - 1, 0);
+    this.date3 = new Date(this.today.getFullYear(), this.today.getMonth() - 2, 0);
+    this.date4 = new Date(this.today.getFullYear(), this.today.getMonth() - 3, 0);
+    this.date5 = new Date(this.today.getFullYear(), this.today.getMonth() - 4, 0);
   }
+
 
 }
