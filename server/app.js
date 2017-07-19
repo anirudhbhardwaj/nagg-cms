9d82df0146fbc2ffa9b84415dbb82b249ec8c8a3
Anirudh Bhardwaj <anirudh.bhardwaj@nagarro.com>
2017-07-19 15:19:48 +0530

    Added new CORS request header

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'uploads')));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization, Accept, X-XSRF-TOKEN");
   next();
 });
 
