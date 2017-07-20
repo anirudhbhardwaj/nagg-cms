74ce58bc45db2b5865cba2bd78439f14df3588e8
sacgithub <sachin31may@gmail.com>
2017-07-20 15:43:58 +0530

    carousel text added

--- ngg-cms/src/app/carousel/carousel.component.ts
+++ ngg-cms/src/app/carousel/carousel.component.ts
@@ -16,9 +16,9 @@ export class CarouselComponent implements OnInit {
     private addNewSlide() {
          this.slides.push(
             // {image:'/assets/static-images/grand_canyon.jpg',text:'nag 1'},
-            {image:'/assets/static-images/love-us.jpg',text:'nag 1'},
-            {image:'/assets/static-images/disrupters-carousel-desk.jpg',text:'nag 2'},
-            {image:'/assets/static-images/enterprise-agile-desk.jpg',text:'nag 3'}
+            {image:'/assets/static-images/love-us.jpg',text:'nagarro values1'},
+            {image:'/assets/static-images/disrupters-carousel-desk.jpg',text:'nagarro values2'},
+            {image:'/assets/static-images/enterprise-agile-desk.jpg',text:'nagarro values3'}
         );
     }
 
