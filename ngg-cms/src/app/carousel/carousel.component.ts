d8cb64362976efdbfa8d89beccaa0ce76958b708
Pawan Taneja <pawantaneja25@gmail.com>
2017-07-20 17:36:26 +0530

    Carousel captions fix

--- ngg-cms/src/app/carousel/carousel.component.ts
+++ ngg-cms/src/app/carousel/carousel.component.ts
@@ -16,9 +16,9 @@ export class CarouselComponent implements OnInit {
     private addNewSlide() {
          this.slides.push(
             // {image:'/assets/static-images/grand_canyon.jpg',text:'nag 1'},
-            {image:'/assets/static-images/love-us.jpg',text:'nagarro values1'},
-            {image:'/assets/static-images/disrupters-carousel-desk.jpg',text:'nagarro values2'},
-            {image:'/assets/static-images/enterprise-agile-desk.jpg',text:'nagarro values3'}
+            {image:'/assets/static-images/love-us.jpg',text:'To know us is to love is'},
+            {image:'/assets/static-images/disrupters-carousel-desk.jpg',text:'Are you losing out to disrupters?'},
+            {image:'/assets/static-images/enterprise-agile-desk.jpg',text:'Enterprise Agile'}
         );
     }
 
